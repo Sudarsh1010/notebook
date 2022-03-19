d73ad0cfc076db5d81b8cdf0c8d5872a4da9cd74
Sudarsh <55168611+Sudarsh1010@users.noreply.github.com>
2022-03-19 09:36:15 +0530

    db and express setup

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require("mongoose");
+const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+
+  })
+}
+
+modele.exports = connectToMongo;
